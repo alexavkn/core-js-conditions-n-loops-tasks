177b5c957b896b8c5b4ea2f4a811d525b43fa669
Averkin Alexander <alexavkn@gmail.com>
2024-01-28 18:08:11 +0300

    add: conditions-n-loops-tasks canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -62,8 +62,16 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (
+    queen.x === king.x ||
+    queen.y === king.y ||
+    (queen.x - king.x < 0 ? -(queen.x - king.x) : queen.x - king.x) ===
+      (queen.y - king.y < 0 ? -(queen.y - king.y) : queen.y - king.y)
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
